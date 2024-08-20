bf0c5809f9c8271e06330d54372c75161e85fd69
khazarglzd <khazarguluzade@gmail.com>
2024-08-21 01:26:47 +0400

    creating first setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Generator from './components/Generator'
+import Hero from './components/Hero'
+import Workout from './components/Workout'
 
 function App() {
 
   return (
     <>
-      <p>
-        Fitness App
-      </p>
+      <Hero />
+      <Generator />
+      <Workout />
     </>
   )
 }
