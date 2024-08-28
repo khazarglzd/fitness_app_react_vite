bd70cb10684205bdcf1e0acd612e7f12d1c36a21
khazarglzd <khazarguluzade@gmail.com>
2024-08-28 16:41:25 +0400

    Creating Hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Generator from './components/Generator'
 import Hero from './components/Hero'
 import Workout from './components/Workout'
@@ -6,11 +5,11 @@ import Workout from './components/Workout'
 function App() {
 
   return (
-    <>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
       <Hero />
       <Generator />
       <Workout />
-    </>
+    </main>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div>Hero</div>
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
+                <p>IT'S TIME TO GET</p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Monster<span className='text-blue-400'>Ator</span></h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>I have full confidence that I want to look like a <span className='text-blue-400'>MONSTER</span>  and that I will do whatever I need to do without fail.</p>
+            <button className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+                <p>Accept & Let's GOO</p>
+            </button>
+        </div>
     )
 }
