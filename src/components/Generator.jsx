0fd3828617f09305a49eb59a3275a72c0fd6a3b5
khazarglzd <khazarguluzade@gmail.com>
2024-08-31 00:52:10 +0400

    creating Generator component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
+import { SCHEMES, WORKOUTS } from '../utils/workoutBanks'
+import Button from './Button'
+
+
+function Header(props) {
+    const { index, title, description } = props
+    return (
+        <div className='flex flex-col gap-4'>
+            <div className='flex items-center justify-center gap-2'>
+                <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+                <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+            </div>
+            <p className='text-sm sm:text-base mx-auto'>{description}</p>
+        </div>
+    )
+}
 
 export default function Generator() {
     return (
         <SectionWrapper id={'generate'} header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
-
+            <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."} />
+            <div className='grid grid-cols-2   border border-solid border-blue-400 sm:grid-cols-4 gap-4'>
+            </div>
+            <Header index={'02'} title={'Lock on targets'} description={"Select the muscles judged for annihilation."} />
+            <div className='bg-slate-950  border border-solid border-blue-400 rounded-lg flex flex-col'>
+            </div>
+            <Header index={'03'} title={'Become Juggernaut'} description={"Select your ultimate objective."} />
+            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+            </div>
+            <Button text={"Formulate"}></Button>
         </SectionWrapper>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './Button'
 
 export default function Hero() {
     return (
@@ -8,9 +9,8 @@ export default function Hero() {
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Monster<span className='text-blue-400'>Ator</span></h1>
             </div>
             <p className='text-sm md:text-base font-light'>I have full confidence that I want to look like a <span className='text-blue-400'>MONSTER</span>  and that I will do whatever I need to do without fail.</p>
-            <button className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
-                <p>Accept & Let's GOO</p>
-            </button>
+            <Button text={"Accept & Let's GOO"}>
+            </Button>
         </div>
     )
 }
